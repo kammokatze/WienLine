435b389b65d19b2a6099494cba875ddf0142a2c5
kammokatze <kamokomokams@gmail.com>
2025-04-08 08:35:10 +0200

    Set up Flask backend with test route

--- backend/app.py
+++ backend/app.py
@@ -1,1 +1,10 @@
- 
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return jsonify({"message": "WienLine Sight Seeing Planner Backend is running!"})
+
+if __name__ == "__main__":
+    app.run(debug=True)
